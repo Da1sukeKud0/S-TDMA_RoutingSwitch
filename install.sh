de1776ddd58f900c23c7c109dc247344010c65f7
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-12-22 20:20:33 +0900

    [add] install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p ~/trema
+mv S-TDMA_RoutingSwitch ~/trema/routing_switch
+cd ~/trema/routing_switch
+bundle install --binstubs
--- lib/test/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p ~/trema
-cd ~/trema
-git clone https://github.com/Da1sukeKud0/S-TDMA_RoutingSwitch.git
-mv S-TDMA_RoutingSwitch routing_switch
-cd routing_switch
-bundle install --binstubs
